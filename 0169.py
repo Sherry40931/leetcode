156ec1227299875517a9d7dab285fc69720fef61
Sherry40931 <sherry18030@gmail.com>
2022-03-22 22:20:45 +0800

    modify 168

--- 0169.py
+++ 0169.py
@@ -10,6 +10,7 @@ from collections import Counter
 class Solution:
     @timing
     def majorityElement(self, nums: List[int]) -> int:
+        # time: O(n), space: O(n/2)
         cnt = defaultdict(lambda: 0)
         half = len(nums) // 2
         for n in nums:
@@ -20,8 +21,24 @@ class Solution:
 
     @timing
     def majorityElement2(self, nums: List[int]) -> int:
-        counts = Counter(nums)
+        # time: O(n), space: O(n/2)
+        counts = Counter(nums)  # return a dict
         return max(counts.keys(), key=counts.get)
+
+    @timing
+    def majorityElement3(self, nums: List[int]) -> int:
+        # time: O(n), space: O(1)
+        # https://leetcode.com/problems/majority-element/discuss/51613/O(n)-time-O(1)-space-fastest-solution
+        ans, cnt = nums[0], 0
+        for n in nums:
+            if cnt == 0:
+                ans = n
+                cnt += 1
+            elif n == ans:
+                cnt += 1
+            else:
+                cnt -= 1
+        return ans
 
 
 print(Solution().majorityElement([3, 2, 3]))
@@ -30,3 +47,6 @@ print(Solution().majorityElement([1] * 10 ** 5 + [2] * 10 ** 3))
 print(Solution().majorityElement2([3, 2, 3]))
 print(Solution().majorityElement2([2, 2, 1, 1, 1, 2, 2]))
 print(Solution().majorityElement2([1] * 10 ** 5 + [2] * 10 ** 3))
+print(Solution().majorityElement3([3, 2, 3]))
+print(Solution().majorityElement3([2, 2, 1, 1, 1, 2, 2]))
+print(Solution().majorityElement3([1] * 10 ** 5 + [2] * 10 ** 3))
