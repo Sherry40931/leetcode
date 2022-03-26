bc1401aa9f299d31782fea42ff5240e5c1899265
Sherry40931 <sherry18030@gmail.com>
2022-03-26 15:29:08 +0800

    update timer

--- timer.py
+++ timer.py
@@ -4,7 +4,8 @@ from time import time
 def timing(fn):
     def wrap(self, *args):
         t1 = time()
-        fn(self, *args)
+        result = fn(self, *args)
         t2 = time()
         print(f'{fn.__name__}: {t2 - t1}')
+        return result
     return wrap
